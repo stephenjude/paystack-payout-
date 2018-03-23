067fedaacc6d1eca0e20f00b22a04d6739b6aa4a
Stephen Jude <31182887+stephenjude@users.noreply.github.com>
2018-03-23 10:23:12 +0100

    Make call to server to verify payment

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
 
 function payWithPaystack() {
-    var handler = PaystackPop.setup({
-        key: 'your_public_key', //remember to use your own paystack public key
-        email: 'customer@email.com',
-        amount: 370000,
-        ref: '' + Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+
+    var handler = PaystackPop.setup({ 
+        key: 'your_public_key', //put your public key here
+        email: 'customer@email.com', //put your customer's email here
+        amount: 370000, //amount the customer is supposed to pay
         metadata: {
             custom_fields: [
                 {
                     display_name: "Mobile Number",
                     variable_name: "mobile_number",
-                    value: "+2348012345678"
+                    value: "+2348012345678" //customer's mobile number
                 }
             ]
         },
         callback: function (response) {
-            alert('success. transaction ref is ' + response.reference);
+            //after the transaction have been completed
+            //make post call  to the server with to verify payment 
+            //using transaction reference as post data
+            $.post("verify.php", {reference:response.reference}, function(status){
+                if(status == "success")
+                    //successful transaction
+                    alert('Transaction was successful');
+                else
+                    //transaction failed
+                    alert(response);
+            });
         },
         onClose: function () {
+            //when the user close the payment modal
             alert('Transaction cancelled');
         }
     });
-    handler.openIframe();
+    handler.openIframe(); //open the paystack's payment modal
 }
