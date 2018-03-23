631b5e33f622e1a51617932a0a833c85ab682b00
Stephen Jude <31182887+stephenjude@users.noreply.github.com>
2018-03-23 22:46:52 +0100

    Minor fix on check post request and comments

--- verify.php
+++ verify.php
@@ -1,15 +1,14 @@
 <?php
-$ref = "";
-if(isset($_POST['reference'])){
-  $ref = $_POST['reference'];
-}else{
+//check if request was made with the right data
+if(!$_SERVER['REQUEST_METHOD'] == 'POST' || !isset($_POST['reference'])){  
   die("Transaction reference not found");
 }
 
-$result = array();
-//The parameter after verify/ is the transaction reference to be verified
-$url = 'https://api.paystack.co/transaction/verify/'.$ref;
+//set reference to a variable @ref
+$reference = $_POST['reference'];
 
+//The parameter after verify/ is the transaction reference to be verified
+$url = 'https://api.paystack.co/transaction/verify/'.$reference;
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -17,8 +16,13 @@ curl_setopt(
   $ch, CURLOPT_HTTPHEADER, [
     'Authorization: Bearer secret_key']
 );
+
+//send request
 $request = curl_exec($ch);
+//close connection
 curl_close($ch);
+//declare an array that will contain the result 
+$result = array();
 
 if ($request) {
     $result = json_decode($request, true);
